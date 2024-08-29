d6fb39d3b6f6b32626114105fe2dc885368a2e09
systemspool <jose.anaya1989@gmail.com>
2024-08-28 22:06:28 -0600

    Se implementa la opcion de eliminar y una alerta para confirmar

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { NumericFormat } from "react-number-format";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function ListadoEmpleados() {
 
+
+
   //definimos un atributo que sera la url base del back
   const urlBase = "http://localhost:8080/rh-app/empleados";
+
 
   //definimos un arreglo para se utilizara para estar pendiente de cuaquier cambio un aarreglo empleado y su set para poder mdoficar su valor y 
   //usamos el hok de usestate para monitorear los cambio y se sincronize con la vista
@@ -27,6 +31,72 @@ export default function ListadoEmpleados() {
     setEmpleados(resultado.data); //con el metodo set vamos a cargar los datos que recibimos (resultado) y los agregamos a nuestro arreglo de empleados
   }
 
+  const eliminarEmpleado = async (id) => {
+    await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.delete(`${urlBase}/${id}`);
+        cargarEmpleados();
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        //recargamos llamando al metodo cargar empleados para que actualice 
+      }
+    });
+    cargarEmpleados();
+  }
+
+    //  const eliminarEmpleado = async (id) => {
+    //    const swalWithBootstrapButtons = Swal.mixin({
+    //      customClass: {
+    //        confirmButton: "btn btn-success",
+    //        cancelButton: "btn btn-danger",
+    //      },
+    //      buttonsStyling: false,
+    //    });
+    //    await
+    //    swalWithBootstrapButtons
+    //      .fire({
+    //        title: "Are you sure?",
+    //        text: "You won't be able to revert this!",
+    //        icon: "warning",
+    //        showCancelButton: true,
+    //        confirmButtonText: "Yes, delete it!",
+    //        cancelButtonText: "No, cancel!",
+    //        reverseButtons: true,
+    //      })
+    //      .then((result) => {
+    //        if (result.isConfirmed) {
+    //    axios.delete(`${urlBase}/${id}`);
+    //          swalWithBootstrapButtons.fire({
+    //            title: "Deleted!",
+    //            text: "Your file has been deleted.",
+    //            icon: "success",
+    //          });
+    //        } else if (
+    //          /* Read more about handling dismissals below */
+    //          result.dismiss === Swal.DismissReason.cancel
+    //        ) {
+    //          swalWithBootstrapButtons.fire({
+    //            title: "Cancelled",
+    //            text: "Your imaginary file is safe :)",
+    //            icon: "error",
+    //          });
+    //        }
+    //      });
+    //             cargarEmpleados();
+    //  }
+  
+
   return (
     //aqui se usa classname para usar bootstrap
     <div className="container">
@@ -41,29 +111,49 @@ export default function ListadoEmpleados() {
             <th scope="col">Departamento</th>
             <th scope="col">Sueldo</th>
             <th scope="col">Editar</th>
+            <th scope="col">Eliminar</th>
           </tr>
         </thead>
         <tbody>
           {
             //Iteramos el arreglo de empleados
             empleados.map((empleado, indice) => (
-                <tr key={indice}>
+              <tr key={indice}>
                 <th scope="row">{empleado.id}</th>
                 <td>{empleado.nombre}</td>
                 <td>{empleado.departamento}</td>
-                <td><NumericFormat value={empleado.sueldo}  //se importa numeric forma instalandolo con react-number-format y se le da formato al varlo de sueldo
-                displayType={"text"}
-                thousandSeparator=',' prefix={'$'}
-                decimalScale={2} fixedDecimalScale></NumericFormat>
-                  </td>
+                <td>
+                  <NumericFormat
+                    value={empleado.sueldo} //se importa numeric forma instalandolo con react-number-format y se le da formato al varlo de sueldo
+                    displayType={"text"}
+                    thousandSeparator=","
+                    prefix={"$"}
+                    decimalScale={2}
+                    fixedDecimalScale
+                  ></NumericFormat>
+                </td>
                 <td>
                   <div>
-                    <Link to={`/editar/${empleado.id}`} className="btn btn-warning btn-sm">Editar</Link>
-                    </div>
+                    <Link
+                      to={`/editar/${empleado.id}`}
+                      className="btn btn-warning btn-sm"
+                    >
+                      Editar
+                    </Link>
+                  </div>
                 </td>
-            </tr>
+                <td>
+                  <div>
+                    <button
+                     onClick={() => eliminarEmpleado(empleado.id)}
+                      className="btn btn-danger btn-sm"
+                    >Eliminar
+                    </button>
+                  </div>
+                </td>
+              </tr>
             ))
-            }
+          }
         </tbody>
       </table>
     </div>
